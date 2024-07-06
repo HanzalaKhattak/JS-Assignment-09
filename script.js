a7f8d7d8e2d519337d06ee152df7eb1cc75ca292
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-07-06 19:20:28 +0500

    add Solutions

---
+++ script.js
@@ -0,0 +1,22 @@
+// TABLE PRINT KARE
+// DESCRIPTION: user se koi number le and uska table 10 tk print kare
+
+// var num = +prompt("Enter a Number to generate a table.")
+// for(var i = 0; i < 10; i++) {
+//     console.log(`${num} *  ${i} = ${num * i}`)
+// }
+
+// 2. ARRAY ELEMENTS KA SUM PRINT KARE
+// DESCRIPTION:
+// 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// 2. phr array elements ka sum find kar k print kare
+
+// const arr = [];
+// for(var i = 0; i < 5; i++) {
+//     arr.push(+prompt("Enter The Number :" ));
+// }
+// var sum = 0;
+// for(var i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(`The sum of the Number in the Array is :  ${sum}`)
